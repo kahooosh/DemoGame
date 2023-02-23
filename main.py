fedd263a958aa09811a6a19a39831d64b9c2cd97
Kailie Hernandez <kailie0805@icloud.com>
2023-02-23 12:45:39 -0800

    commit

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ pygame.init()
 #Creating Display 
 screen = pygame.display.set_mode((800,400)) # Creates window((width,height))
 pygame.display.set_caption('Demo Game') #Title of Window
-pygame.time.Clock()
 
 #Draw all elements and update everything
 while True: 
@@ -17,5 +16,3 @@ while True:
             pygame.quit
             exit()
     pygame.display.update()
-    clock.tick(60) 
-
