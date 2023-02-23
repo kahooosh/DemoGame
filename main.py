f06c2bfba718c8c61b3eafac6793349bde774cf0
Kailie Hernandez <kailie0805@icloud.com>
2023-02-23 13:32:29 -0800

    COMMIT

--- main.py
+++ main.py
@@ -6,9 +6,13 @@ from sys import exit
 pygame.init()
 
 #Creating Display 
-screen = pygame.display.set_mode((800,400)) # Creates window((width,height))
+screen = pygame.display.set_mode((800,400)) #Creates window ((width,height))
 pygame.display.set_caption('Demo Game') #Title of Window
-clock = pygame.time.Clock()
+clock = pygame.time.Clock('Red')
+
+#Surfaces 
+test_surface = pygame.Surface((100,200)) #((w,h))
+test_surface.fill() #Adds color 
 
 #Draw all elements and update everything
 while True: 
@@ -16,5 +20,8 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit
             exit()
+
+    screen.blit(test_surface, (0,0)) 
+    #Blit = Block image transfer, one surface on another; arguement = (surface, pos)
     pygame.display.update()
-    clock.tick(60)
+    clock.tick(60) #sets maximum frame rate (fps)
