f43aefa820997c9bd0da1d9112cc88deefe74a04
Kailie Hernandez <kailie0805@icloud.com>
2023-02-23 11:31:47 -0800

    commit

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
+#BASIC SET UP FOR GAME 
+
+#Importing and initiallizing pygame 
+import pygame, sys 
+from sys import exit 
+pygame.init()
+
+#Creating Display 
+screen = pygame.display.set_mode((800,400)) # Creates window((width,height))
+pygame.display.set_caption('Demo Game') #Title of Window
+pygame.time.Clock()
+
+#Draw all elements and update everything
+while True: 
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit
+            exit()
+    pygame.display.update()
+    clock.tick(60) 
 
