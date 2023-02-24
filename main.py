47a0333492c4d65b5e427f01bc3c801d261370fd
Kailie Hernandez <kailie0805@icloud.com>
2023-02-23 21:11:00 -0800

    commit

--- main.py
+++ main.py
@@ -10,8 +10,12 @@ screen = pygame.display.set_mode((800,400)) #Creates window ((width,height))
 pygame.display.set_caption('Demo Game') #Title of Window
 clock = pygame.time.Clock()
 
+#Surfaces and Fonts 
+test_font = pygame.font.Font(None,50 ) #(type, size)
 sky_surface = pygame.image.load('graphics/Sky.png')
 ground_surface = pygame.image.load('graphics/ground.png')
+text_surface = test_font.render('Demo Game', False, 'Green') #(text, AA, color )
+#AA: anti-aliasing; smoothing edges of the text (put true for non-pixel and false for other)
 
 """
 #Surfaces Example 
@@ -26,8 +30,10 @@ while True:
             pygame.quit
             exit()
 
+#Blit = Block image transfer, one surface on another; arguement = (surface, pos)
     screen.blit(sky_surface, (0,0)) # (0,0) is top left 
     screen.blit(ground_surface, (0,300))
-    #Blit = Block image transfer, one surface on another; arguement = (surface, pos)
+    screen.nlot(text_surface, (300,50))
+
     pygame.display.update()
-    clock.tick(60) #sets maximum frame rate (fps)
+    clock.tick(60) #sets maximum frame rate (fps )
