00ae63d8f7025704f20185aa5e5d78296c8a462a
Kailie Hernandez <kailie0805@icloud.com>
2023-02-25 18:57:27 -0800

    c

--- main.py
+++ main.py
@@ -113,9 +113,9 @@ while True:
         if score == 0:
             screen.blit(game_title,game_title_rect)
 
-        elif score != 0:
+        else:
             score_message = test_font.render(f'Your score: {score}',False,(111,196,169))
-            score_message_rect = score_message.get_rect(center = (400,120))
+            score_message_rect = score_message.get_rect(center = (400,100))
             screen.blit(score_message,score_message_rect)
             screen.blit(game_over,game_over_rect)
 
