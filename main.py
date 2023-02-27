49350fe61d0e921724053b4aac5a37aa74ba4ae2
Kailie Hernandez <kailie0805@icloud.com>
2023-02-27 12:52:30 -0800

    c

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #Importing and initiallizing pygame 
 import pygame, sys 
 from sys import exit 
+from random import randint
 pygame.init()
 
 def display_score():
@@ -9,6 +10,17 @@ def display_score():
     score_rect = score_surf.get_rect(center = (400,50))
     screen.blit(score_surf,score_rect)
     return current_time
+
+def obstacle_movement(obstacle_list): 
+    if obstacle_list:
+        for obstacle_rect in obstacle_list:
+            obstacle_rect.c -= 5 
+
+            screen.blit(en1_surf,obstacle_rect
+        
+        return obstacle_list)
+    else:
+        return []
 
 
 #Creating Display 
@@ -32,24 +44,24 @@ test_font = pygame.font.Font('graphics/Pixeltype.ttf',50) #(type, size)
 sky_surf = pygame.image.load('graphics/Sky.png').convert()
 ground_surf = pygame.image.load('graphics/ground.png').convert()
 
-#Game Title: Pixel Runner 
+#Intro/Game Over Screen
 game_title = test_font.render('Pixel Runner',False,(37,56,74))
 game_title = pygame.transform.rotozoom(game_title,0,2)
 game_title_rect = game_title.get_rect(center = (400,60))
 
-#Game Over 
 game_over = test_font.render('GAME OVER',False,(37,56,74))
 game_over = pygame.transform.rotozoom(game_over,0,2)
 game_over_rect = game_over.get_rect(center = (400,60))
 
-#Press Space 
 press_space = test_font.render('Press space bar to restart',False,'White')
 press_space = pygame.transform.rotozoom(press_space,0,0.80)
 press_space_rect = press_space.get_rect(center = (400,330))
 
-#Enemy One 
+#Obstacles 
 en1_surf = pygame.image.load('graphics/snail1.png').convert_alpha() #enemy 1 
 en1_rect = en1_surf.get_rect(bottomright = (600,300))
+
+obstacle_rect_list = []
 
 #Player 
 player_surf = pygame.image.load('graphics/player_walk_1.png') #player
@@ -62,6 +74,10 @@ player_stand = pygame.image.load('graphics/player_stand.png').convert_alpha()
 player_stand = pygame.transform.rotozoom(player_stand,0,1.5) #(surf,rotation,scale); scales and rotate 
 player_stand_rect = player_stand.get_rect(center = (400,200))
 
+#Timer 
+obstacle_timer = pygame.USEREVENT + 1
+pygame.time.set_timer(obstacle_timer,900) #(event we want to trigger, how often to trigger in millisec.)
+
 
 #DRAW ALL ELEMENTS AND UPDATE
 while True: 
@@ -69,6 +85,7 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit
             exit()
+
         if game_active:
             if event.type == pygame.MOUSEBUTTONDOWN: #pos of mouse when moving 
                 if player_rect.collidepoint(event.pos) and player_rect.bottom >= 300: 
@@ -82,6 +99,9 @@ while True:
                 en1_rect.left = 800
                 start_time = int(pygame.time.get_ticks()/1000)
 
+        if event.type == obstacle_timer and game_active:
+            obstacle_rect_list.append(en1_surf.get_rect(bottomright = (randint(900 - 1100),300)))
+
     if game_active:
         #Putting Surfaces and Rectangles on Display 
         screen.blit(sky_surf, (0,0)) # (0,0) is top left 
@@ -94,11 +114,13 @@ while True:
         """
         score = display_score()
 
+        """
         #Enemy_1 Movememnt 
         en1_rect.x -= 4  
         if en1_rect.right <= 0: en1_rect.left = 800 
         screen.blit(en1_surf,en1_rect) 
-        """ Another way to move en1: 
+
+        Another way to move en1: 
         en1_x_pos -= 4 #moves to left every loop
         if en1_x_pos < -100: en1_x_pos = 800 #brings back en1 to the right side 
         """
@@ -109,6 +131,10 @@ while True:
         if player_rect.bottom >= 300:
             player_rect.bottom = 300
         screen.blit(player_surf,player_rect)
+
+        #Obstacle  Movement
+        obstacle_rect_list = obstacle_movement(obstacle_rect_list)
+
 
         #Collisions
         if en1_rect.colliderect(player_rect): 
